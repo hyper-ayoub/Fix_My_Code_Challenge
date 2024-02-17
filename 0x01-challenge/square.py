92bd9e7c3dee2b98141f29b101216935be9fca83
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-17 15:55:18 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
 
 class Square:
-    side_length = 0
+    """
+    This class represents a square.
+    """
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        """
+        Initialize the Square object with width and height.
+        """
+        self.width = width
+        self.height = height
 
-    def area(self):
-        """ Calculate the area of the square """
-        return self.side_length ** 2
+    def area_of_my_square(self):
+        """
+        Calculate the area of the square.
+        """
+        return self.width * self.height
 
-    def perimeter(self):
-        """ Calculate the perimeter of the square """
-        return 4 * self.side_length
+    def perimeter_of_my_square(self):
+        """
+        Calculate the perimeter of the square.
+        """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "Square with side length {}".format(self.side_length)
+        """
+        Return a string representation of the square.
+        """
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = Square(side_length=12)
+    s = Square(width=12, height=9)
     print(s)
-    print("Area:", s.area())
-    print("Perimeter:", s.perimeter())
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
+
